85f9505c3fca1fdb0188bc36212101ee33e6a413
ViniciusTorres14 <131274048+ViniciusTorres14@users.noreply.github.com>
2024-04-03 17:12:03 -0300

    Update Usuario.cs

--- API/api_eventos/Models/Usuario.cs
+++ API/api_eventos/Models/Usuario.cs
@@ -27,8 +27,8 @@ namespace api_eventos.Models
         [Column("perfil")]
         public string? Perfil { get; set;}
 
-        [Column("status")]
-        public string? Status { get; set;}
+        [Column("ativo")]
+        public string? Ativo { get; set;}
 
 
     }
